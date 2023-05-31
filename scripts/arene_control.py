5cc0303c3caf0a64391704511338ac3f93a4d30b
RobLafoo <robin.leveque94@gmail.com>
2023-05-31 10:45:06 +0200

    tout est fonctionnel

--- scripts/arene_control.py
+++ scripts/arene_control.py
@@ -2,53 +2,61 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from gazebo_msgs.srv import ApplyJointEffort, ApplyJointEffortRequest
-from random import randint
-
-def control_traffic_light(color, effort, duration):
-    rospy.wait_for_service('/gazebo/apply_joint_effort')
-    TL_request.joint_name = "base_to_light_{}".format(color)
-    TL_request.effort = effort
-    TL_request.start_time.secs = 0
-    TL_request.duration.secs = duration
-    TL_handle(TL_request)
-
-def to_red():
-    control_traffic_light('red', 1, 1)
-    control_traffic_light('green', -1, 1)
-    
-def to_green():
-    control_traffic_light('red', -1, 1)
-    control_traffic_light('green', 1, 1)
-    
-def orange_blink(n):
-    for i in range(n):
-        rospy.sleep(1)
-        control_traffic_light('orange', 1, 1)
-        rospy.sleep(1)
-        control_traffic_light('orange', -1, 1)
-        
-    
+import click
+from std_msgs.msg import Float64
 
 
-if __name__ == '__main__':
-    rospy.init_node('barrier', anonymous=True) # anonymous=True allows multiple nodes with the same name to be launched
-    rate = rospy.Rate(0.2) # 0.2hz
-    TL_request = ApplyJointEffortRequest()
-    TL_handle = rospy.ServiceProxy('/gazebo/apply_joint_effort',ApplyJointEffort)
-    cmpt = 0
+
+class AreneControl:
+    def __init__(self):
+        self.pub_barrier1 = rospy.Publisher('/barrier_1/gear_to_barrier_1_position_controller/command', Float64, queue_size=10)
+        self.pub_barrier2 = rospy.Publisher('/barrier_2/gear_to_barrier_2_position_controller/command', Float64, queue_size=10)
+        self.pub_light_green = rospy.Publisher('/traffic_lights_1/base_to_light_1_position_controller/command', Float64, queue_size=10)
+        self.pub_light_yellow = rospy.Publisher('/traffic_lights_1/base_to_light_2_position_controller/command', Float64, queue_size=10)
+        self.pub_light_red = rospy.Publisher('/traffic_lights_1/base_to_light_3_position_controller/command', Float64, queue_size=10)
+
+    def to_green(self):
+        self.pub_light_green.publish(1)
+        self.pub_light_yellow.publish(-1)
+        self.pub_light_red.publish(-1)
+
+    def to_red(self):
+        self.pub_light_green.publish(-1)
+        self.pub_light_yellow.publish(-1)
+        self.pub_light_red.publish(1)
+
+    def yellow_blink(self,n):
+        for i in range(n):
+            self.pub_light_green.publish(-1)
+            self.pub_light_yellow.publish(1)
+            self.pub_light_red.publish(-1)
+            rospy.sleep(0.5)
+            self.pub_light_green.publish(-1)
+            self.pub_light_yellow.publish(-1)
+            self.pub_light_red.publish(-1)
+            rospy.sleep(0.5)
+
+
+if __name__=="__main__":
     while not rospy.is_shutdown():
-        cmpt+=1
+        rospy.init_node("arene_control",anonymous=True)
+        AC = AreneControl()
         try:
-            to_green()
-            rospy.sleep(5)
-            orange_blink(3)
-            to_red()
-            rospy.sleep(5)
-            orange_blink(1)
-            to_green()
-            
+            char = click.getchar()
+            if char == 'a':
+                AC.pub_barrier1.publish(0.2)
+            if char == 'q':
+                AC.pub_barrier1.publish(-0.2)
+            if char == 'z':
+                AC.pub_barrier2.publish(0.2)
+            if char == 's':
+                AC.pub_barrier2.publish(-0.2)
+            if char == 'e':
+                AC.to_green()
+            if char == 'r':
+                AC.to_red()
+            if char == 't':
+                AC.yellow_blink(3)
+
         except rospy.ROSInterruptException:
-            pass
-    rospy.spin()
-    
+                pass
